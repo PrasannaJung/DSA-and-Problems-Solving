59950b69414b373eaecf7afdd39816a7475b6190
PrasannaJung <prasannajung08@gmail.com>
2022-12-29 17:48:49 +0545

    Enhancing the bubble sort to only compare the values in remaining array

--- DSA/src/BubbleSort.java
+++ DSA/src/BubbleSort.java
@@ -12,9 +12,20 @@ public class BubbleSort {
         // setting a counter variable i which represents the total no. of elements in the array
         for(int i = 0;i<arr.length;i++){
             // inner loop j where the j keep tracks of the elements to be compared and swapped. It starts from 1 as we compare the current element at j with the previous one and if the current one is small, we swap those values
+            /*
             for(int j = 1;j<arr.length;j++){
                 if(arr[j]<arr[j-1]){
                     // swapping the elements
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+            */
+
+            // we can also enhance the inner loop further by only having to check for the remaining array and not the ones already sorted. Already sorted values end up on the right side
+            for(int j = 1;j < arr.length-1;j++){
+                if(arr[j]<arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
