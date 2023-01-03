88d569b80406f388a3dce0a2be6d4de4d61365ee
PrasannaJung <prasannajung08@gmail.com>
2023-01-03 11:42:11 +0545

    Practiced some more advanced patterns

---
+++ Patterns/src/AdvancedPatterns.java
@@ -0,0 +1,36 @@
+public class AdvancedPatterns {
+    public static void main(String[] args) {
+//        pattern28(5);
+        pattern30(5);
+    }
+
+    static void pattern28(int n){
+        for(int row=1;row<2*n;row++){
+            int cols = row > n ? n - (row -n) : row;
+            int spaces = n - cols;
+            for(int s=1;s<=spaces;s++){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=cols;c++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+
+    static void pattern30(int n){
+        for(int row=1;row<=5;row++){
+            for(int s=1;s<=n-row;s++){
+                System.out.print(" ");
+            }
+            for(int col=row;col>=1;col--){
+                System.out.print(col);
+            }
+            for(int col=2;col<=row;col++){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
